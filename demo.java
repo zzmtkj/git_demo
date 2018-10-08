9b68062a31e53847c092828a5041b6d1c2a63882
nick <474119316@qq.com>
2018-10-08 11:28:41 +0800

    after github.com

--- demo.java
+++ demo.java
@@ -4,6 +4,7 @@ public class demo{
 	public static void main(String[] args) {
 		System.out.println("powered by nick abc");
 		System.out.println("Hello World");
+		System.out.println("This msg is updated after commit github.com");
 		}
 	
 }
